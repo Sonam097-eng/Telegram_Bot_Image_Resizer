8eb74637c2594886c62e59a678eb926101b192a2
Sonam097-eng <sonamvermalt97@gmail.com>
2026-03-24 12:49:08 +0530

    added function
    Signed-off-by: Sonam097-eng <sonamvermalt97@gmail.com>

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -14,9 +14,29 @@ def get_token():
 
 def get_url():
     token= get_token()
-    url= f"https://api.telegram.org/bot{token}"
-    file_url= f"https://api.telegram.org/file/bot{token}"
+    base_url = "https://api.telegram.org"
+    url= f"{base_url}/bot{token}"
+    file_url= f"{base_url}/file/bot{token}"
     return url, file_url
+
+def call_telegram(method, url, data=None, files=None, headers=None, params=None):
+    resp = request(method = method, url= url, data= data, files= files, headers=headers, params=params)
+    return resp
+
+def resizing_image(image_bytes, size_value):
+
+    with Image.open(io.BytesIO(image_bytes))as image:
+        quality= 95
+        while quality > 10:
+            output_buffer= io.BytesIO()
+            image.save(output_buffer,format='JPEG',quality= quality, optimize= True)
+            if len(output_buffer.getvalue()) < size_value:
+                output_buffer.seek(0)
+                break
+            quality -= 5
+            output_buffer.seek(0)
+    return output_buffer       
+    
 
 def lambda_handler(event, context):
     # print(event)
@@ -32,17 +52,20 @@ def lambda_handler(event, context):
             "chat_id":chat_id,
             "text":"Please send me photo!I resizes images only"
         }
-        send_photo_list_resp= request(method= "GET",url= text_url, data= text_payload)
-        if send_photo_list_resp.status_code not in [200, 201, 203, 204]:
-            return{"message":"Response of image not found", "status_code":500}
-        return{"message":"image not found", "status_code":200}
+        resp_data = call_telegram("GET", text_url, data=text_payload)
 
+        if not resp_data.get("status"):
+                return{"message": resp_data.get("resp"), "status_code": 500 if not resp_data.get("status_code") else resp_data.get("status_code")}    
+        return {"message":"Image not found", "status_code":200}
+    
+                           
     caption= body.get('message',{}).get('caption')
     if not caption:
         caption_url = f"{url}/send_caption"
         caption_text = "Please send me caption so that i can resize my image"
-        send_caption_resp = request(method="GET", url=caption_url, data=caption_text)
-        if send_caption_resp.status_code not in [200, 201, 203, 204]:
+        
+        resp = call_telegram("GET", caption_url, data=caption_text)
+        if resp.status_code not in [200, 201, 203, 204]:
             return{"message":"send caption response not worked", "status_code":500}
         return{"message":"Dont get caption", "status_code":200}
 
@@ -51,44 +74,32 @@ def lambda_handler(event, context):
     if not match:
             return{"message":"doesnt match with caption", "status_code":400}
     resizing_mb = int(match.group(1))
-    print(f"message:got value{resizing_mb}, status_code:200")
+    print(f"message:got value{resizing_mb}")
     
     if not photo_list or not chat_id:
         return{"message":"neither image nor chat_id found","status_code":400}
-    file_id= photo_list[-1].get('file_id')  
+    file_id = photo_list[-1].get('file_id')  
     url, file_url = get_url()
     file_path_url= f"{url}/getFile?file_id={file_id}"
-    print(f"file_path_url:{file_path_url}")
-    path_resp= request(method="GET",url=file_path_url)
-    print(f"path_response:{path_resp}")
-    path_data= path_resp.json()
-    print(f"path_data:{path_data}")
+    
+    resp = call_telegram("GET", file_path_url)
+    print(f"path_response:{resp}")
+    path_data= resp.json()
+    
     file_path= path_data.get('result',{}).get('file_path')
-    print(f"file_path:{file_path}")
+    
     if not file_path:
          return{f"Not found file:{file_path},status_code:400"}
     
     download_url= f"{file_url}/{file_path}"
-    print(f"download_url:{download_url}")
-    image_resp =request(method="GET",url=download_url)
-    print(f"image_resp:{image_resp}")
-
-    image_bytes= image_resp.content
-   # print(f"image_bytes:{image_bytes}")
-    size_value= 1024*1024
-
-    with Image.open(io.BytesIO(image_bytes))as image:
-        quality= 95
-        while quality > 10:
-            output_buffer= io.BytesIO()
-            image.save(output_buffer,format='JPEG',quality= quality, optimize= True)
-            if len(output_buffer.getvalue()) < size_value:
-                output_buffer.seek(0)
-                break
-            quality -= 5
-            output_buffer.seek(0)    
-            
     
+    resp = call_telegram("GET", download_url)
+    
+    image_bytes= resp.content
+   # print(f"image_bytes:{image_bytes}")
+
+    size_value = resizing_mb
+    output_buffer = resizing_image(image_bytes, size_value )
     send_url= f"{url}/sendPhoto"
     print(f"send_url:{send_url}")
     fields= {
@@ -96,8 +107,8 @@ def lambda_handler(event, context):
         'caption': 'Here is your resized image!'
      }
     files= {'photo':(('image.jpg', output_buffer, 'image/jpeg'))}
-    send_resp= request(method= "POST",url=send_url,data=fields,files= files)
-    print(f"send_resp:{send_resp}")
+    resp= call_telegram("POST", send_url, data=fields, files=files)
+    print(f"send_resp:{resp}")
     return{
         'status_code':200,
         'message':'success'
